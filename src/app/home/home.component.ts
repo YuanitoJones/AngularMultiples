7b01247bb509c5be7e6e3a55f91b03151b0f0a30
Juan Alexandro Renteria Abril <97640975+YuanitoJones@users.noreply.github.com>
2023-05-24 21:21:02 -0700

    angular starting point

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements OnInit {
+  ngOnInit() {}
+
+  // Number to use in input
+  initialNumber = 0;
+  //Array which will contain al multiples
+  multiples: number[] = [];
+
+  CalculateNumber(receivedNumber: number) {
+    const calculatedNumbers: number[] = [];
+    for (let i = 1; i <= receivedNumber; i++) {
+      //Runs from 1 to the input number
+      if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+        //Check if number is divisible by 3, 5 or 7
+        calculatedNumbers.push(i);
+      }
+    }
+    //Assigns array to multiples variable to reassign instead of pushing infinitely
+    this.multiples = calculatedNumbers;
+  }
+
+  CheckSmallerMultiple(receivedNumber: number) {
+    //returns color of smallest multiple
+    if (receivedNumber % 3 === 0) return 'green';
+    if (receivedNumber % 5 === 0) return 'red';
+    if (receivedNumber % 7 === 0) return 'blue';
+    return 'white';
+  }
+
+  getMultiples(receivedNumber: number) {
+    const multiples: number[] = [];
+    if (receivedNumber % 3 === 0) multiples.push(3);
+    if (receivedNumber % 5 === 0) multiples.push(5);
+    if (receivedNumber % 7 === 0) multiples.push(7);
+    if (multiples.length > 1) return multiples;
+    return 0;
+  }
+}
